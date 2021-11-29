56e025dc67a86e29973a8cc4d065bc9ed4a86a5d
Schmiels <svenbartos@gmail.com>
2021-11-29 02:31:28 -0500

    completed alphabet

--- send.py
+++ send.py
@@ -12,6 +12,15 @@ if __name__ == "__main__":
         if c.isalpha() and c.isupper():
             key = 'shift+'+c.lower()
             keyboard.send(key)
+        elif c == '=':
+            key = 'shift+0'
+            keyboard.send(key)
+        elif c == '+':
+            key = '+'
+            keyboard.send(key)
+        elif c == '/':
+            key = 'shift+7'
+            keyboard.send(key)
         else:
             keyboard.send(c)
         time.sleep(0.1)
